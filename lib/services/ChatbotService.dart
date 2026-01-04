d311d8656681af7dc23ddf372c6b75dbfd163c6d
anhthuong <accngonnheae@gmail.com>
2026-01-04 22:17:55 +0700

    update truoc khi merge

--- lib/services/ChatbotService.dart
+++ lib/services/ChatbotService.dart
@@ -3,7 +3,7 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 
 class ChatbotService {
 
-
+  static const String _apiKey = 'AIzaSyAdwX_WJ-6YAVz34H-rVJ72ssi0d_OyOYc';
 
   late final GenerativeModel _model;
   ChatSession? _chatSession;
